20f53e349183481a9799fdeed0cd7499c7780cbe
BHASVIC-WilliamWinter24 <william.winter24@students.bhasvic.ac.uk>
2025-09-29 12:34:34 +0100

    Stage 2 Complete

--- Cannonfall/Assets/Scripts/Cannonball.cs
+++ Cannonfall/Assets/Scripts/Cannonball.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class Cannonball : MonoBehaviour
 {
-    /*
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Ground"))
-            Destroy(gameObject);
-    }*/
     void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ground"))
+        if (collision.CompareTag("Ground") || collision.CompareTag("Hazard"))
             Destroy(gameObject);
     }
 }
